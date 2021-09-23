94df520b508d43f508d88463504e091c15771cde
Tom Witkowski <tom.witkowski@frischepost.de>
2021-09-24 00:44:24 +0200

    fix first mutation

--- tests/Feature/Eloquent/Builders/MovieBuilder/FindManyTest.php
+++ tests/Feature/Eloquent/Builders/MovieBuilder/FindManyTest.php
@@ -3,46 +3,58 @@
 use Astrotomic\Tmdb\Models\Movie;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 
-it('creates multiple movies from tmdb', function (): void {
-    $movies = Movie::query()->findMany([335983, 575788]);
+it('creates multiple movies from tmdb', function ($ids): void {
+    $movies = Movie::query()->findMany($ids);
 
     expect($movies)
         ->toBeInstanceOf(EloquentCollection::class)
         ->toHaveCount(2)
         ->each->toBeCreatedModel(Movie::class);
-});
+})->with([
+    [[335983, 575788]],
+    collect([335983, 575788]),
+]);
 
-it('creates movie from tmdb and ignores not found', function (): void {
-    $movies = Movie::query()->findMany([335983, 0]);
+it('creates movie from tmdb and ignores not found', function ($ids): void {
+    $movies = Movie::query()->findMany($ids);
 
     expect($movies)
         ->toBeInstanceOf(EloquentCollection::class)
         ->toHaveCount(1)
         ->each->toBeCreatedModel(Movie::class, 335983);
-});
+})->with([
+    [[335983, 0]],
+    collect([335983, 0]),
+]);
 
-it('creates movie from tmdb and finds movie in database', function (): void {
+it('creates movie from tmdb and finds movie in database', function ($ids): void {
     Movie::query()->find(335983);
-    $movies = Movie::query()->findMany([335983, 575788]);
+    $movies = Movie::query()->findMany($ids);
 
     expect($movies)
         ->toBeInstanceOf(EloquentCollection::class)
         ->toHaveCount(2)
         ->find(335983)->toBeRetrievedModel(Movie::class, 335983)
         ->find(575788)->toBeCreatedModel(Movie::class, 575788);
-});
+})->with([
+    [[335983, 575788]],
+    collect([335983, 575788]),
+]);
 
-it('finds multiple movies in database', function (): void {
+it('finds multiple movies in database', function ($ids): void {
     Movie::query()->find(335983);
     Movie::query()->find(575788);
-    $movies = Movie::query()->findMany([335983, 575788]);
+    $movies = Movie::query()->findMany($ids);
 
     expect($movies)
         ->toBeInstanceOf(EloquentCollection::class)
         ->toHaveCount(2)
         ->find(335983)->toBeRetrievedModel(Movie::class, 335983)
         ->find(575788)->toBeRetrievedModel(Movie::class, 575788);
-});
+})->with([
+    [[335983, 575788]],
+    collect([335983, 575788]),
+]);
 
 it('returns empty collection without ids', function (): void {
     $movies = Movie::query()->findMany([]);
