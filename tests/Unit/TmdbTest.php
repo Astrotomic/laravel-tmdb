806c793b196a70d705198efda1152ce7c72d9cca
Tom Witkowski <tom.witkowski@frischepost.de>
2022-04-20 09:58:05 +0200

    add tests for tmdb class

---
+++ tests/Unit/TmdbTest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+use Astrotomic\Tmdb\Tmdb;
+
+it('can use a given language', function(): void {
+    $tmdb = new Tmdb();
+
+    expect($tmdb->useLanguage('de'))
+        ->language()->toBe('de');
+});
+
+it('can use a given region', function(): void {
+    $tmdb = new Tmdb();
+
+    expect($tmdb->useRegion('DE'))
+        ->region()->toBe('DE');
+});
