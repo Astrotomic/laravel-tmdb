95bffb12a9bb1fbaf29b6dc6e334f6b74feb0cd5
Tom Witkowski <tom.witkowski@frischepost.de>
2022-04-20 10:21:55 +0200

    fix php cs

--- tests/Unit/TmdbTest.php
+++ tests/Unit/TmdbTest.php
@@ -1,16 +1,15 @@
 <?php
 
-
 use Astrotomic\Tmdb\Tmdb;
 
-it('can use a given language', function(): void {
+it('can use a given language', function (): void {
     $tmdb = new Tmdb();
 
     expect($tmdb->useLanguage('de'))
         ->language()->toBe('de');
 });
 
-it('can use a given region', function(): void {
+it('can use a given region', function (): void {
     $tmdb = new Tmdb();
 
     expect($tmdb->useRegion('DE'))
